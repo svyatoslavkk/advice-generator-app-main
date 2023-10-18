e8cd7b7f3b9b3a3f0a365ffbc6b80208cb4a7f06
svyatoslavkk <sv.krivonos1@gmail.com>
2023-10-18 13:56:38 +0300

    feat: first-mobile done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.scss';
+import dividerMobile from './pattern-divider-mobile.svg';
+import dividerDesktop from './pattern-divider-desktop.svg';
+import iconButton from './icon-dice.svg';
 
 function App() {
   return (
     <div className='container'>
-
+      <div className='content-block'>
+        <h1 className='title'>Advice #117</h1>
+        <p className='advice-text'>"It is easy to sit up and take notice, what's difficult is getting up and taking action."</p>
+        <img className='divider-img' src={dividerMobile} alt="Divider Image" />
+        {/* <img className='divider-img' src={dividerDesktop} alt="Divider Image" /> */}
+        <div className='button-block'>
+          <img className='icon-button' src={iconButton} alt="Icon Button" />
+        </div>
+      </div>
     </div>
   );
 }
