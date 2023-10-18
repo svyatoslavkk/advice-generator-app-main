6fb8571adec804688e76caaee1e641109cdb6c13
svyatoslavkk <sv.krivonos1@gmail.com>
2023-10-18 16:02:19 +0300

    feat: desktop done, styled components has been used and native api request done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import dividerMobile from './pattern-divider-mobile.svg';
 import dividerDesktop from './pattern-divider-desktop.svg';
 import iconButton from './icon-dice.svg';
+import { Button } from './Components/Button';
 
 function App() {
+  const [advice, setAdvice] = useState('');
+
+  const fetchAdvice = async () => {
+    fetch('https://api.adviceslip.com/advice')
+      .then((response) => response.json())
+      .then((data) => {
+        const adviceData = data.slip.advice
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
     <div className='container'>
       <div className='content-block'>
         <h1 className='title'>Advice #117</h1>
         <p className='advice-text'>"It is easy to sit up and take notice, what's difficult is getting up and taking action."</p>
-        <img className='divider-img' src={dividerMobile} alt="Divider Image" />
-        {/* <img className='divider-img' src={dividerDesktop} alt="Divider Image" /> */}
-        <div className='button-block'>
+        <img className='divider-img mobile' src={dividerMobile} alt="Divider Image" />
+        <img className='divider-img desktop' src={dividerDesktop} alt="Divider Image" />
+        <Button>
           <img className='icon-button' src={iconButton} alt="Icon Button" />
-        </div>
+        </Button>
       </div>
     </div>
   );
---
+++ src/Components/Button.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    border: none;
+    outline: none;
+    border-radius: 50%;
+    background-color: hsl(150, 100%, 66%);
+    min-width: 66px;
+    min-height: 66px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    bottom: -33px;
+    left: 50%;
+    transform: translateX(-50%);
+    cursor: pointer;
+    transition: 0.1s ease;
+    &:hover,
+    &:active,
+    &:focus {
+        box-shadow: 0 0 36px hsl(150, 100%, 66%);
+    }
+`;
